7449e81193091be55775ddeef335a81854f74c0b
Екатерина Ковалева <yekaterina.kovaleva@on.kase.kz>
2021-05-06 10:24:24 +0600

    sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let sortedArray = arr.slice();
+  if (param == 'asc') {
+    sortedArray.sort(compareAsc);
+  } else {
+    sortedArray.sort(compareDesc);
+  }
+  return sortedArray;
+}
 
+function compareAsc(a, b) {
+  return a.localeCompare(b,'ru', { caseFirst : 'upper'} );
+}
+
+function compareDesc(a, b) {
+  return b.localeCompare(a,'ru', { caseFirst : 'upper'} );
 }
