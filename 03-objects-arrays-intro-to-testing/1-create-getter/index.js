2c1b399ebe9f7a1c83193edd7087a2ac38e6af12
Yekaterina <dkat0612@gmail.com>
2021-05-12 10:10:13 +0600

    createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const props = path.split('.');
+  let i = 0;
+  return (obj) => {
+    while (i < props.length && typeof obj === 'object') {
+      const map = new Map(Object.entries(obj));
+      obj = map.get(props[i++]);
+    }
+    return obj;
+  }
 }
